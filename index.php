fcbbec67b6f415768c61f05e079e38d90b45f761
mdarifmia <arif@supremesupports.com.au>
2023-12-29 21:34:01 +0600

    class, object, properties, method with dynamic data

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-class Man{
+
+// class, object, method with static data
+/*class Man{
     public $name = "Arif";
     public $age = '24';
     public $gender = "male";
@@ -16,6 +18,28 @@ $my_info = new Man();
 echo $my_info->name;
 echo $my_info->age;
 echo $my_info->gender;
-echo $my_info->info();
+echo $my_info->info();*/
 
+
+// class, object, method with dynamic data
+/*
+class Man{
+    public $name;
+    public $age;
+    public $gender;
+
+    function info() {
+        return "Name: ".$this->name.",\n"."Age: ".$this->age."\n"."Gender: ".$this->gender;
+    }
+}
+
+
+$my_info = new Man();
+
+$my_info->name = "Arif";
+$my_info->age = "24";
+$my_info->gender = "Male";
+
+echo $my_info->info();
+*/
 ?>
